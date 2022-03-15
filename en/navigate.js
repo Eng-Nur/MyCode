6643c6739018025fb74a4b3627eeff4f7a0234da
Eng.Nur <101563498+Eng-Nur@users.noreply.github.com>
2022-03-15 09:35:58 +0600

    Add files via upload

---
+++ en/navigate.js
@@ -0,0 +1,10 @@
+export default {
+    home: 'Home',
+    history: 'History',
+    profile: 'Profile',
+    support: 'Support',
+    settings: 'Settings',
+    help:'Help & FAQ’s',
+    logout:'LOG OUT',
+    sendmoney:'Send Money',
+  }

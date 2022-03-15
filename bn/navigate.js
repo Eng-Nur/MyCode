9c26abb7a6deb30f548e5814c300a4745077b41d
Eng.Nur <101563498+Eng-Nur@users.noreply.github.com>
2022-03-15 09:36:20 +0600

    Add files via upload

---
+++ bn/navigate.js
@@ -0,0 +1,10 @@
+export default {
+    home: 'হোম',
+    history: 'পূর্বের লেনদেন',
+    profile: 'প্রোফাইল',
+    support: 'সাপোর্ট',
+    settings: 'সেটিংস',
+    help:'সাহায্য এবং FAQ’s',
+    logout:'লগ-আউট',
+    sendmoney:'টাকা পাঠান',
+  };
